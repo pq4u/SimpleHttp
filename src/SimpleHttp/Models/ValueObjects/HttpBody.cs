23f78dc1a294cc28ed0d99023340772a2f813d32
axl <axl@PABLITO>
2025-08-06 22:23:46 +0200

    Add HttpBody value object

---
+++ src/SimpleHttp/Models/ValueObjects/HttpBody.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace SimpleHttp.Models.ValueObjects;
+
+public record HttpBody
+{
+    public byte[] Content { get; }
+    private HttpBody(byte[] value) => Content = value;
+    public static HttpBody Empty => new(Array.Empty<byte>());
+    public static HttpBody FromText(string text) => new(Encoding.UTF8.GetBytes(text));
+    public string AsText() => Encoding.UTF8.GetString(Content);
+    public int Length => Content.Length;
+}
