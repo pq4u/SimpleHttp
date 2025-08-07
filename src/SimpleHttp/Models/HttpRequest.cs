b201b172064a665c3df2a9b2d4037573b13f2dc0
axl <axl@PABLITO>
2025-08-07 12:01:50 +0200

    Add HttpRequest class

---
+++ src/SimpleHttp/Models/HttpRequest.cs
@@ -0,0 +1,13 @@
+﻿using SimpleHttp.Models.ValueObjects;
+using HttpMethod = SimpleHttp.Models.ValueObjects.HttpMethod;
+
+namespace SimpleHttp.Models;
+
+public class HttpRequest
+{
+    public HttpMethod Method { get; set; }
+    public HttpPath Path { get; set; }
+    public HttpVersion Version { get; set; }
+    public Dictionary<HeaderName, HeaderValue> Headers { get; set; } = new();
+    public HttpBody Body { get; set; }
+}
